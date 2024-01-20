e89c5372665588845c2842565b5109349b603252
Israel <piresr.israel@gmail.com>
2024-01-20 12:06:54 -0300

    chore(api): configure eslint airbnb style guide

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'prettier'
   ],
   root: true,
   env: {
@@ -21,5 +24,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
   },
 };
