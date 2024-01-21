39c5be01e3c0dd7eed5b42aee7ee5deda21a8a9a
Israel <piresr.israel@gmail.com>
2024-01-21 11:04:04 -0300

    fix(web): adjust eslint config

--- web/components/atoms/Search.tsx
+++ web/components/atoms/Search.tsx
@@ -1,12 +1,11 @@
 import { InputText } from "primereact/inputtext";
 import React from "react";
 
-
-export default function Search(): React.ReactNode {  
+export default function Search(): React.ReactNode {
   return (
     <span className="p-input-icon-left">
       <i className="pi pi-search" />
       <InputText placeholder="Keyword Search" />
     </span>
-  )
+  );
 }
