4d9a633e72a4e3daca52b123aeec4aa2d4e06f80
Israel <piresr.israel@gmail.com>
2024-01-21 22:42:36 -0300

    chore(api): configure cors and validation pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { runSeeders } from 'typeorm-extension';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { logger } from './shared/utils/logger';
 import typeormConfig from './infrastructure/databases/typeorm/configs/typeorm.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
 
   const dataSource = await typeormConfig.initialize();
   await runSeeders(dataSource);
